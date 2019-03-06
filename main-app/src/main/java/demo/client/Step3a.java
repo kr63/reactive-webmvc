63867440be27489a14cbfd418421d39be5f65501
Rustam Karimov <rkarimov.r@gmail.com>
2019-03-06 15:17:07 +0400

    Complete steps: 2c, 2d, 2e and 3a.

---
+++ main-app/src/main/java/demo/client/Step3a.java
@@ -0,0 +1,20 @@
+package demo.client;
+
+import demo.Person;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class Step3a {
+
+    private static WebClient client = WebClient.create("http://localhost:8081");
+
+
+    public static void main(String[] args) {
+
+        client.get().uri("/persons/events")
+                .retrieve()
+                .bodyToFlux(Person.class)
+                .take(4)
+                .blockLast();
+    }
+
+}
